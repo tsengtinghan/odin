bd5a10dc7282be8bc78607e48d3e586157ee2515
Michael Tseng <michael08120@gmail.com>
2024-06-10 00:05:31 -0700

    jina api

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [url, setUrl] = useState<string>("");
+
+  const handleClick = async () => {
+    const response = await fetch(`https://r.jina.ai/${url}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const response_json = await response.json();
+    const article = response_json.data;
+    console.log(article);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-4">
       <h1 className="text-center text-xl">Save articles</h1>
       <div className="flex space-between gap-4">
-        <Input placeholder="url" className="w-72" />
-        <Button>Submit</Button>
+        <Input
+          value={url}
+          placeholder="url"
+          className="w-72"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <Button onClick={handleClick}>Submit</Button>
       </div>
     </div>
   );
