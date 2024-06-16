d2f4298d9434292e75762f9bacf66bc1f735c427
Michael Tseng <michael08120@gmail.com>
2024-06-16 00:15:53 -0700

    sticky navbar!

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 import { Home, BookmarkPlus } from "lucide-react";
 export default function Navbar() {
   return (
-    <div className="w-auto h-20 flex items-center justify-center">
-      <div className="flex flex-row space-x-16">
-        <Link href="/feed">
-          <Home />
-        </Link>
-        <Link href="/dashboard">
-          <BookmarkPlus />
-        </Link>
+    <header className="bg-white sticky top-0 w-full">
+      <div className="w-auto h-20 flex items-center justify-center">
+        <div className="flex flex-row space-x-16">
+          <Link href="/feed">
+            <Home />
+          </Link>
+          <Link href="/dashboard">
+            <BookmarkPlus />
+          </Link>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
